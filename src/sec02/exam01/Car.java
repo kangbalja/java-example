acb22b781967615da82f078975f9301a779ba7f9
kangbalja <kangbalja@gmail.com>
2020-05-09 21:26:49 +0900

    Update Car class

--- src/sec02/exam01/Car.java
+++ src/sec02/exam01/Car.java
@@ -6,4 +6,16 @@ public class Car {
 	String color = "검정";
 	int maxSpeed = 350;
 	int speed;
+	
+	Tire frontLeftTire = new HankookTire();
+	Tire frontRightTire = new HankookTire();
+	Tire backLeftTire = new HankookTire();
+	Tire backRightTire = new HankookTire();
+	
+	void run() {
+		frontLeftTire.roll();
+		frontRightTire.roll();
+		backLeftTire.roll();
+		backRightTire.roll();
+	}
 }
